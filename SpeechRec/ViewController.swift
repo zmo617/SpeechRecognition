c8e13e34c63be9e542a37022046c070924a1ab57
Zongying Mo <zm3998@utexas.edu>
2020-06-08 21:30:03 -0500

    Initial Commit

---
+++ SpeechRec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRec
+//
+//  Created by Claire Mo on 6/8/20.
+//  Copyright © 2020 Zongying Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
